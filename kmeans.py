0be9fd92abfe8845a3e1ba4c56c5c0c1fba6e8f3
YZW-explorer <74363764+YZW-explorer@users.noreply.github.com>
2021-05-27 16:54:57 +0800

    Add files via upload

---
+++ kmeans.py
@@ -0,0 +1,23 @@
+import joblib
+import numpy
+from matplotlib import pyplot  as plt
+from sklearn.metrics import accuracy_score
+import numpy
+import seaborn as sns; sns.set()
+from sklearn.datasets import make_blobs
+patient_idx_lat_lon = joblib.load('./patient_idx_lat_lon.pkl')
+lat_lon_info = numpy.empty([0, 2])
+for key, val in patient_idx_lat_lon.items():
+    temp_lat_lon = numpy.array(val).reshape(1, 2)
+    lat_lon_info = numpy.concatenate((lat_lon_info, temp_lat_lon), axis=0)
+
+plt.scatter(lat_lon_info[:,0], lat_lon_info[:, 1], s=50)
+from sklearn.cluster import KMeans
+kmeans = KMeans(n_clusters=4)
+kmeans.fit(lat_lon_info)
+y_kmeans = kmeans.predict(lat_lon_info)
+plt.scatter(lat_lon_info[:, 0], lat_lon_info[:, 1], c=y_kmeans, s=50, cmap='viridis')
+
+centers = kmeans.cluster_centers_
+plt.scatter(centers[:,0], centers[:, 1], c='black', s=80, marker='x')
+plt.show()
